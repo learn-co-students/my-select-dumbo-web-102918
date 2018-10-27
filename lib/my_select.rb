477cfc1bade4f1baabfce3189d8ec29fc5bda766
Bruce Hopewell <lbhopewell@outlook.com>
2018-10-27 03:58:59 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,21 @@
-def my_select(collection)
- # your code here!
+require "pry"
+def my_select(num)
+ new_array = []
+ i = 0 
+ while i < num.length 
+ yield(num[i])
+if yield(num[i]) == true
+  new_array << num[i]
+ end
+ i = i + 1
+ end
+new_array
 end
+
+
+array = [1, 2, 3, 4, 5] 
+my_select(array) do |number| 
+  number.even?
+#binding.pry
+end
+
